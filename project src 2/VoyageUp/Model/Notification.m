afd946f040d3328272e3ce82902040740100e7bc
Pankaj Kunwar <pankaj_kunwar1988@yahoo.com>
2015-10-12 21:53:45 +0530

    Updating organization name in each file

--- project src 2/VoyageUp/Model/Notification.m
+++ project src 2/VoyageUp/Model/Notification.m
@@ -3,7 +3,7 @@
 //  VoyageUp
 //
 //  Created by Deepak on 31/05/15.
-//  Copyright (c) 2015 Deepak. All rights reserved.
+//  Copyright (c) 2015 Erbauen Labs. All rights reserved.
 //
 
 #import "Notification.h"
--- project src 2/VoyageUp/ViewControllers/SignUpViewController.h
+++ project src 2/VoyageUp/ViewControllers/SignUpViewController.h
@@ -3,7 +3,7 @@
 //  VoyageUp
 //
 //  Created by Deepak on 09/05/15.
-//  Copyright (c) 2015 Deepak. All rights reserved.
+//  Copyright (c) 2015 Erbauen Labs. All rights reserved.
 //
 
 
